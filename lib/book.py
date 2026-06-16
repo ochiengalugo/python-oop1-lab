768da47cd31d36de2d26e1c56a5c624218d18115
ochiengalugo <ochieng.alugo@student.moringaschool.com>
2026-06-16 22:59:37 +0300

    updated

--- lib/book.py
+++ lib/book.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
+
 class Book:
-    def __init__(self, title: str, page_count: int):
-        # Initializing instance attributes
+    def __init__(self, title, page_count):
         self.title = title
-        self.page_count = page_count  # Uses the setter below for validation
+        # Setting this via the property setter ensures validation happens at initialization
+        self.page_count = page_count 
 
     @property
     def page_count(self):
@@ -10,16 +12,12 @@ class Book:
 
     @page_count.setter
     def page_count(self, value):
-        # Validation: Ensure it is an integer
         if not isinstance(value, int):
-            print("If not print 'page_count must be an integer'")
-            # Depending on your precise test file requirements, you might need to raise an error:
-            # raise TypeError("page_count must be an integer")
+            print("page_count must be an integer")
             return
         self._page_count = value
 
     def turn_page(self):
-        # Method to simulate turning a page
-        print("Will print 'Flipping the page...wow, you read fast!'")
+        print("Flipping the page...wow, you read fast!")
     
         
