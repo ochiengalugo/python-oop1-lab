9b00f2db141021a235e837851d1da48e29363371
ochiengalugo <ochieng.alugo@student.moringaschool.com>
2026-06-16 23:08:57 +0300

    updated

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 class Book:
+    pass
+
     def __init__(self, title, page_count):
         self.title = title
         self.page_count = page_count  # Triggers the setter validation
@@ -18,3 +20,5 @@ class Book:
 
     def turn_page(self):
         print("Flipping the page...wow, you read fast!")
+    
+        
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 class Coffee:
+    pass
+
     def __init__(self, size, price):
         self.size = size  # Triggers the setter validation
         self.price = price
